f1eb1de0347918423372c3615d67af6b0a840796
Harvey Zheng <zheng.harvey5@gmail.com>
2014-08-30 16:20:55 -0400

    Subjects and article edit/delete/new done

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -8,5 +8,45 @@ class ArticlesController < ApplicationController
 		@topic = @subtopic.topic
 		@section = @topic.id
 	end
+  def new
+    @article = Article.new
+    @article.subject_id = params['subject_id']
+  end
+  def create
+    @article = Article.new(article_params)
+    @subtopic = @article.subject.subtopic
+    if @article.save
+      flash[:success] = "Article successfully created!"
+      redirect_to subtopic_path(@subtopic)
+    else
+      render 'new'
+    end
+  end
+  def destroy
+    @article = Article.find(params[:id])
+    @subtopic = @article.subject.subtopic
+    @subject = @article.subject
+    @article.destroy
+    flash[:success] = "Article destroyed."
+    redirect_to subtopic_path(@subtopic)
+  end
+  def edit
+    @article = Article.find(params[:id])
+  end
+  def update
+    @article = Article.find(params[:id])
+    if @article.update_attributes(article_params)
+      flash[:success] = "Article name updated"
+      redirect_to article_path(@article)
+    else
+      render 'edit'
+    end
+  end
+
+  private
+
+  def article_params
+    params.require(:article).permit(:title, :subject_id, :description, :link_id)
+  end
 
 end
--- app/controllers/subjects_controller.rb
+++ app/controllers/subjects_controller.rb
@@ -7,4 +7,45 @@ class SubjectsController < ApplicationController
 		@section = @topic.id
 		@articles = @subject.articles
 	end
+  def new
+    @subject = Subject.new
+    @subject.subtopic_id = params['subtopic_id']
+  end
+  def create
+    @subject = Subject.new(subject_params)
+    @subtopic = @subject.subtopic
+    if @subject.save
+      flash[:success] = "Subject successfully created!"
+      redirect_to subtopic_path(@subtopic)
+    else
+      render 'new'
+    end
+  end
+  def destroy
+    @subject = Subject.find(params[:id])
+    @subtopic = @subject.subtopic
+    @subject.destroy
+    flash[:success] = "Subject destroyed."
+    redirect_to subtopic_path(@subtopic)
+  end
+  def edit
+    @subject = Subject.find(params[:id])
+  end
+  def update
+    @subject = Subject.find(params[:id])
+    @subtopic = @subject.subtopic
+    if @subject.update_attributes(subject_params)
+      flash[:success] = "Subtopic name updated"
+      redirect_to subtopic_path(@subtopic)
+    else
+      render 'edit'
+    end
+  end
+
+  private
+
+  def subject_params
+    params.require(:subject).permit(:name, :subtopic_id)
+  end
+
 end
