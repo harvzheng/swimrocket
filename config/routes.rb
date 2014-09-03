c7c8e12d7119af09902eebed0ebb5026f9b3f6b3
Harvey Zheng <zheng.harvey5@gmail.com>
2014-09-02 21:55:44 -0400

    New Article's breadcrumb kinda working

--- config/routes.rb
+++ config/routes.rb
@@ -25,9 +25,9 @@ SampleApp::Application.routes.draw do
   get "static_pages/about"
   get "static_pages/contact"
 
+  resources :articles
   resources :subtopics
   resources :topics
-  resources :articles
   resources :subjects
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
