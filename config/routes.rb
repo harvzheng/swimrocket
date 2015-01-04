41391d55d20fd29ee079b94fd316d71209386cf9
Harvey Zheng <zheng.harvey5@gmail.com>
2015-01-03 20:25:27 -0500

    bug fixes
    -switched breadcrumbs & title
    -restyled some things that looked weird

--- config/routes.rb
+++ config/routes.rb
@@ -8,15 +8,15 @@ SampleApp::Application.routes.draw do
   match '/signin',  to: 'sessions#new',         via: 'get'
   match '/signout', to: 'sessions#destroy',     via: 'delete'
 
-  match '/help',    to: 'static_pages#help',    via: 'get'
-  match '/about',   to: 'static_pages#about',   via: 'get'
-  match '/contact', to: 'static_pages#contact', via: 'get'
+  # match '/help',    to: 'static_pages#help',    via: 'get'
+  # match '/about',   to: 'static_pages#about',   via: 'get'
+  # match '/contact', to: 'static_pages#contact', via: 'get'
 
 
   get "static_pages/home"
-  get "static_pages/help"
-  get "static_pages/about"
-  get "static_pages/contact"
+  # get "static_pages/help"
+  # get "static_pages/about"
+  # get "static_pages/contact"
 
   resources :articles
   resources :subtopics
